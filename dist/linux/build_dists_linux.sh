5eb72e468c2c0862a42cf0ce2f139e443f8c8057
Luca Steccanella <steccas@pm.me>
2021-07-18 16:39:04 +0200

    dist files update

--- dist/linux/build_dists_linux.sh
+++ dist/linux/build_dists_linux.sh
@@ -2,10 +2,10 @@
 
 chmod -R 755 ./proton-client
 dpkg-deb --build proton-client
-mv proton-client.deb proton-client-1.2.0-amd64.deb
+mv proton-client.deb proton-client-1.2.1-amd64.deb
 sudo debtap -u
-debtap ./proton-client-1.2.0-amd64.deb
-sudo alien -r ./proton-client-1.2.0-amd64.deb --scripts
-zip -rv proton-client-1.2.0-1-x86_64.arch.zip .INSTALL .MTREE .PKGINFO proton-client-1.2.0-1-x86_64.pkg.tar.zst
+debtap ./proton-client-1.2.1-amd64.deb
+sudo alien -r ./proton-client-1.2.1-amd64.deb --scripts
+zip -rv proton-client-1.2.1-1-x86_64.arch.zip .INSTALL .MTREE .PKGINFO proton-client-1.2.1-1-x86_64.pkg.tar.zst
 
 exit 0
